b24b6f7129dc57015f5904d39932986f50bd7b07
Joon Chung <joonsubtalk@gmail.com>
2013-12-06 13:18:09 -0800

    Added Kat's information and look/feel

---
+++ phpsource/function/footer.php
@@ -0,0 +1,35 @@
+		<footer>
+          <div class="squeeze">
+            <div class="row bump">
+              <div class="span4">
+                <h4>More Links</h4>
+                <ul class="nav">
+                  <li class="active"><a href="#">Home</a></li>
+                  <li><a href="../services">SRC Services</a></li>
+                  <li><a href="#">UCLA Scholarships</a></li>
+                  <li><a href="#">Calendar</a></li>
+                  <li><a href="#">National and Int'l Merit Scholarships</a></li>
+                  <li><a href="#">Search Services</a></li>
+                  <li><a href="#">Resources and Support</a></li>
+                </ul>
+             </div>
+             <div class="span4">
+                <h4>Contact Us</h4>
+                <address>
+                  <strong>Scholarship Resource Center</strong><br>
+                  233 Covel Commons<br />
+                  Los Angeles, CA 90095-7247<br />
+                  <abbr title="Phone">P:</abbr> (310) 206-2875
+                </address>
+             </div>
+             <div class="span4">
+                <h4>Connect with Us</h4>
+                <p><a href="#">Facebook</a></p>
+                <p><a href="#">Twitter</a></p>
+                <p><a href="#">Google Plus</a></p>
+             </div>
+            </div>
+            <p class="pull-right"><a href="#"><i class="icon-arrow-up"></i>Back to top</a></p>
+            <p><span class="muted">&copy; 2013 Scholarship Resource Center. All rights reserved.</span></p>
+          </div>
+        </footer>
